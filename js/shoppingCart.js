b0fe19fa7733acc223c1e825d9a4b35579f5c978
Sumon Mondle <sumonmondle@gmail.com>
2023-02-15 12:34:54 +0600

    2nd js file added

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -37,24 +37,25 @@ function updateCaseNumber(isIncrease) {
     return newCaseNumber;
 }
 
+function updateCaseTotalPrice(newCaseNumber) {
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+}
+
 
 document.getElementById('btn-case-plus').addEventListener('click', function(){
    const newCaseNumber = updateCaseNumber(true);
 
-   const caseTotalPrice = newCaseNumber * 59;
-   const caseTotalElement = document.getElementById('case-total');
-   caseTotalElement.innerText = caseTotalPrice;
+   updateCaseTotalPrice(newCaseNumber);
+  
 })
 
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
     const newCaseNumber = updateCaseNumber(false);
 
-    const caseTotalPrice = newCaseNumber * 59;
-    const caseTotalElement = document.getElementById('case-total');
-    caseTotalElement.innerText = caseTotalPrice;
-
-
+    updateCaseTotalPrice(newCaseNumber);
 })
 
 
